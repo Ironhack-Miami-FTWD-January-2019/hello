b7ff61b13a1e45653d04193d2254d9183300a7a9
Tzikas <niko.tzikas@gmail.com>
2019-03-20 18:30:25 -0400

    s

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const { isLoggedIn } = require('../middlewares')
 const router = express.Router();
+const User = require('../models/User')
 
-
+console.log('innnnn herrrrre')
 
 router.get("/whatever", (req, res, next) => {
-  console.log('in whatever',req.user)
-  res.json({user:req.user})
+  console.log('123456 in whatever',req.user)
+  User.find().then(allUsers=>{
+    res.json({user:req.user, allUsers:allUsers})
+  })
 })
 
 
